1b42347b9f2454712336a1ca2fd36e29acad4807
Y Sai Koushik Reddy <152732336+KoushikReddy9963@users.noreply.github.com>
2024-11-15 22:13:47 +0530

    Payement setup

--- backend/index.js
+++ backend/index.js
@@ -9,26 +9,57 @@ import employeeRoutes from './routes/employeeRoutes.js';
 import cors from 'cors';
 import AdvertisementRoutes from './routes/AdvertisementRoutes.js';
 import feedbackRoutes from './routes/feedbackroutes.js';
-dotenv.config();
+import Stripe from 'stripe';
 
+dotenv.config();
+const stripe = new Stripe('sk_test_51QFqmE04LmtyqkXBjqvuWUaFlhLtHefd6sH1bMlVnz3oz628S1DGj3qVB0Xl2p3PIsD4CrU34xpQ6svZkcDnlkgf00A98gKu96');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
-// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/buyer', buyerRoutes);
 app.use('/api/seller', sellerRoutes);
 app.use('/api/employee', employeeRoutes);
 app.use('/api/adv', AdvertisementRoutes);
-app.use('/api/feedback',feedbackRoutes);
+app.use('/api/feedback', feedbackRoutes);
+
+app.post('/api/seller/payment', async (req, res) => {
+    try {
+        const { amount, currency, product } = req.body;
+
+        // console.log('amount:', amount);
+        // console.log('currency:', currency);
+        // console.log('product:', product);
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: currency || 'usd',
+                        product_data: { name: product || 'Real Estate Payment' },
+                        unit_amount: amount,
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: `http://localhost:3000/payment-success`,
+            cancel_url: `http://localhost:3000/payment-cancel`,
+
+        });
+        res.json({ paymentUrl: session.url, flag: true });
+    } catch (error) {
+        console.error('Error creating Stripe session:', error);
+        res.status(500).json({ error: 'Payment initiation failed' });
+    }
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
