58d595d5d233b04d8751276d2b9d1864afb691bf
HimasagarU <164536315+HimasagarU@users.noreply.github.com>
2025-05-03 23:38:30 +0530

    Updated swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: 'http://real-estate-delta-tawny.vercel.app/api',
       },
     ],
     components: {
