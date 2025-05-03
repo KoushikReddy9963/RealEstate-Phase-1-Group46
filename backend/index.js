b8122a2e887b7c2ba1baaf5904982b3def4f4589
Y Sai Koushik Reddy <152732336+KoushikReddy9963@users.noreply.github.com>
2025-05-03 14:27:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://Koushik:1234@koushik.xbrofez.mongodb.net/Realestate")
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
@@ -23,7 +23,7 @@ import buyerRoutes from './routes/buyerRoutes.js';
 import sellerRoutes from './routes/sellerRoutes.js';
 import employeeRoutes from './routes/employeeRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-import advertisementRoutes from './routes/advertisementRoutes.js';
+import AdvertisementRoutes from './routes/AdvertisementRoutes.js';
 import feedbackroutes from './routes/feedbackroutes.js';
 
 
@@ -32,7 +32,7 @@ app.use('/api/buyer', buyerRoutes);
 app.use('/api/seller', sellerRoutes);
 app.use('/api/employee', employeeRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/adv', advertisementRoutes);
+app.use('/api/adv', AdvertisementRoutes);
 app.use('/api/feedback', feedbackroutes);
 
 app.use((err, req, res, next) => {
